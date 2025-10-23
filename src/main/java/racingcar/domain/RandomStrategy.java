fa5e6186a48c1f71b67bacceada30117bfa3c780
2jaeheon <jaeheon0826@jbnu.ac.kr>
2025-10-23 18:36:51 +0900

    feat(strategy): 자동차 이동 전략을 설정
    자동차에 대한 이동 전략을 설정
    
    세부 사항:
    - MoveStrategy 인터페이스의 shouldMove 메서드 설정
    - RandomStrategy에서 shouldMove를 오버라이딩해서 구현

--- src/main/java/racingcar/domain/RandomStrategy.java
+++ src/main/java/racingcar/domain/RandomStrategy.java
@@ -1,4 +1,15 @@
 package racingcar.domain;
 
-public class RandomStrategy implements MoveStrategy{
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomStrategy implements MoveStrategy {
+    private static final int MOVE_THRESHOLD = 4;
+    private static final int MAX_RANDOM_NUMBER = 10;
+
+
+    @Override
+    public boolean shouldMove() {
+        int random = Randoms.pickNumberInRange(0, MAX_RANDOM_NUMBER - 1);
+        return random > MOVE_THRESHOLD;
+    }
 }
