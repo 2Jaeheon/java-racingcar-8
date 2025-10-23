b0e2e3dbc9745c50f788257c110f8ee9e1b65dea
2jaeheon <jaeheon0826@jbnu.ac.kr>
2025-10-23 14:09:50 +0900

    feat(input): 자동차 이름 입력
    자동차 입력 프롬프트 메시지를 출력하고, 자동차 정보를 입력받음

--- src/main/java/racingcar/domain/GameConsole.java
+++ src/main/java/racingcar/domain/GameConsole.java
@@ -1,4 +1,22 @@
 package racingcar.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class GameConsole {
+
+    private static final String CAR_NAMES_PROMPT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String ROUND_PROMPT = "시도할 횟수는 몇 회인가요?";
+
+    private String read() {
+        return Console.readLine();
+    }
+
+    private void print(String message) {
+        System.out.println(message);
+    }
+
+    public void play() {
+        print(CAR_NAMES_PROMPT);
+        String carNames = read();
+    }
 }
